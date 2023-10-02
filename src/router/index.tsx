d40e0bc5658438e7b65b87b1a42805da562b0e69
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-10-02 21:30:12 +0800

    chore: backup

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -14,63 +14,66 @@ import Stat from '@/pages/question/stat/Stat'
 import Home from '@/pages/home/Home'
 import Login from '@/pages/login/Login'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'register',
-        element: <Register />,
-      },
-      {
-        path: 'manage',
-        element: <ManageLayout />,
-        children: [
-          {
-            path: 'list',
-            element: <List />,
-          },
-          {
-            path: 'star',
-            element: <Star />,
-          },
-          {
-            path: 'trash',
-            element: <Trash />,
-          },
-        ],
-      },
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'login',
+          element: <Login />,
+        },
+        {
+          path: 'register',
+          element: <Register />,
+        },
+        {
+          path: 'manage',
+          element: <ManageLayout />,
+          children: [
+            {
+              path: 'list',
+              element: <List />,
+            },
+            {
+              path: 'star',
+              element: <Star />,
+            },
+            {
+              path: 'trash',
+              element: <Trash />,
+            },
+          ],
+        },
 
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-  },
-  {
-    path: 'question',
-    element: <QuestionLayout />,
-    children: [
-      {
-        path: 'edit/:id',
-        element: <Edit />,
-      },
-      {
-        path: 'stat/:id',
-        element: <Stat />,
-      },
-    ],
-  },
-])
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+    },
+    {
+      path: 'question',
+      element: <QuestionLayout />,
+      children: [
+        {
+          path: 'edit/:id',
+          element: <Edit />,
+        },
+        {
+          path: 'stat/:id',
+          element: <Stat />,
+        },
+      ],
+    },
+  ],
+  { basename: '/survey-sage' },
+)
 
 export default router
 
