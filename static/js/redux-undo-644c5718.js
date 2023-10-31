8be53eb499be7229b0055c600ef8ecced1c0d060
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-10-31 14:00:10 +0000

    Deploying to gh-pages from @ rayadaschn/survey-sage@e2cac552df0a19632e407a4bfdc3b37a99d4a454 🚀

--- static/js/redux-undo-644c5718.js
+++
@@ -1,1 +0,0 @@
-const e="@@redux-undo/UNDO",u="@@redux-undo/REDO",d="@@redux-undo/JUMP_TO_FUTURE",o="@@redux-undo/JUMP_TO_PAST",n="@@redux-undo/JUMP",r="@@redux-undo/CLEAR_HISTORY",t={undo:()=>({type:e}),redo:()=>({type:u}),jumpToFuture:e=>({type:d,index:e}),jumpToPast:e=>({type:o,index:e}),jump:e=>({type:n,index:e}),clearHistory:()=>({type:r})};export{t as M};
